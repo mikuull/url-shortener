feb38d9482bf26bbd3d881f352ef9b6df37aa3be
mikuull <mmikulski643@gmail.com>
2024-02-03 13:37:50 +0100

    feat: UrlForm component structure and styles

--- src/components/UrlContainer/UrlForm/UrlForm.tsx
+++ src/components/UrlContainer/UrlForm/UrlForm.tsx
@@ -1,11 +1,25 @@
+import Button from "@/components/Button";
 import React from "react";
 
 const UrlForm = () => {
   return (
     <>
-      <div className="flex justify-center items-center">
-        <h1 className="text-4xl font-bold uppercase">URL SHORTENER</h1>
-        <form></form>
+      <div className="flex flex-col justify-center items-center space-y-4">
+        <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 uppercase">
+          URL SHORTENER
+        </h1>
+        <form className="flex flex-col justify-center items-center space-y-4">
+          <input
+            type="text"
+            placeholder="Paste the link"
+            className="p-3 rounded-xl bg-[#4f4f4f] text-[#ffffff] placeholder-white"
+          />
+          <Button
+            href="/"
+            title="Submit"
+            property="bg-gradient-to-r from-purple-400 to-pink-600"
+          />
+        </form>
       </div>
     </>
   );
