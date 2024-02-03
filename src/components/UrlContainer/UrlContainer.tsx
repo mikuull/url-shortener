c955988961243173902b322f33cc9a5cdcc2134a
mikuull <mmikulski643@gmail.com>
2024-02-03 13:22:52 +0100

    feat: UrlContainer component

---
+++ src/components/UrlContainer/UrlContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UrlForm from "./UrlForm/UrlForm";
+
+const UrlContainer = () => {
+  return (
+    <>
+      <UrlForm />
+    </>
+  );
+};
+
+export default UrlContainer;
