0fcab4442b1e22ea57de438f83877f0face87351
mikuull <mmikulski643@gmail.com>
2024-02-03 13:23:06 +0100

    feat: UrlForm component

---
+++ src/components/UrlContainer/UrlForm/UrlForm.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const UrlForm = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <h1 className="text-4xl font-bold uppercase">URL SHORTENER</h1>
+        <form></form>
+      </div>
+    </>
+  );
+};
+
+export default UrlForm;
